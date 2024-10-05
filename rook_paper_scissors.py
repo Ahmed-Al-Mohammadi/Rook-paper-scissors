c71c28db077a14d45e1151e3c3eaec9dcc107304
Ahmed-Al-Mohammadi <ahmedyassersayed2024@gmail.com>
2024-10-05 19:47:00 +0300

    Update rook_paper_scissors.py

--- rook_paper_scissors.py
+++ rook_paper_scissors.py
@@ -1,30 +1,26 @@
 import random
-
-ROCK = 1
-PAPER = 2
-SCISSORS = 3
+choice = ["ROCK", "PAPER", "SCISSORS"]
 
 PLAYER_WINS = "Player wins!! Woop woop!"
 COMPUTER_WINS = "Robocop wins :-("
 TIE = "It's a tie!"
 
 def rock_paper_scissors(player_choice):
-    computer_choice = random.choice([ROCK, PAPER, SCISSORS])
-    choices = {ROCK: "Rock", PAPER: "Paper", SCISSORS: "Scissors"}
+    computer_choice = random.choice(choice)
     
-    print(f"Player chose: {choices[player_choice]}")
-    print(f"Computer chose: {choices[computer_choice]}")
+    print(f"Player chose: {player_choice}")
+    print(f"Computer chose: {computer_choice}")
     
     if player_choice == computer_choice:
         print(TIE)
         return TIE
-    elif (player_choice == ROCK and computer_choice == SCISSORS) or \
-         (player_choice == PAPER and computer_choice == ROCK) or \
-         (player_choice == SCISSORS and computer_choice == PAPER):
+    elif (player_choice == "ROCK" and computer_choice == "SCISSORS") or \
+         (player_choice == "PAPER" and computer_choice == "ROCK") or \
+         (player_choice == "SCISSORS" and computer_choice == "PAPER"):
         print(PLAYER_WINS)
         return PLAYER_WINS
     else:
         print(COMPUTER_WINS)
         return COMPUTER_WINS
-x = int(input("choise num between 1 to 3"))        
+x = input("choise your play : ").upper()       
 rock_paper_scissors(x)        
